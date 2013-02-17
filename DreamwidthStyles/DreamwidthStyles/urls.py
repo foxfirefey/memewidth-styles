31ca2290902fe063f788350deaf7a9393c07da45
Foxfirefey <foxfirefey@gmail.com>
2013-02-17 23:16:28 +0000

    Found the error in importing DWStyles app urls; fixed it.

--- DreamwidthStyles/DreamwidthStyles/urls.py
+++ DreamwidthStyles/DreamwidthStyles/urls.py
@@ -3,7 +3,9 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+from DWStyles.views import DWLayoutListView
+
 urlpatterns = patterns('',
-    url(r'^$', include('DWStyles.urls', namespace="dwstyles", app_name="DWStyles")),
+    url(r'', include('DWStyles.urls', namespace="dwstyles", app_name="DWStyles")),
     url(r'^admin/', include(admin.site.urls)),
 )
